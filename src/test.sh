de94c55fcd6e82219593469c471ebccc6a1e331b
Aya Khaled <ayakhaledelshenawy@gmail.com>
2026-02-11 17:33:28 +0200

    Fix test file

--- src/test.sh
+++ src/test.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 
-#src/tet.sh
 EXPECTED="Hello, Test!"
+OUTPUT=$(node -e "console.log(require('./src/greet')('Test'))")
 
-OUTPUT = $(node -e "console.log(require('./src/app')('Test'))")
-
-if ["$OUTPUT"== "$EXPECTED"]; then
-echo "Test passed!"
-exist 0
-
+if [ "$OUTPUT" == "$EXPECTED" ]; then
+    echo "Test passed!"
+    exit 0
 else
-echo "Test failed! Expected `$EXPECTED` but got '$OUTPUT'"
-
-exist 1
-
+    echo "Test failed! Expected '$EXPECTED' but got '$OUTPUT'"
+    exit 1
 fi
